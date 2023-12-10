54432d28333657662d000e574b5595488ea54e61
Sakshi Shinde <82663545+Sakshi-hub21@users.noreply.github.com>
2023-12-10 15:18:00 +0530

    Update calender.js

--- src/calender.js
+++ src/calender.js
@@ -22,7 +22,7 @@ const CustomEvent = ({ event }) => {
   return (
     <div style={{ display: "flex", flexDirection: "column", height: eventHeight }}>
       <div ref={titleRef}>{event.clientName}</div>
-      <span style={{ fontSize: "0.8em", color: "grey" }}> - {event.time}</span>
+      <div>{event.time}</div>
     </div>
   );
 };
