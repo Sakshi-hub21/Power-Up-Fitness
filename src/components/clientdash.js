566ede80d04c59fe3335014d29404e6de2c35c52
Sakshi Shinde <82663545+Sakshi-hub21@users.noreply.github.com>
2023-12-10 15:41:57 +0530

    Update clientdash.js

--- src/components/clientdash.js
+++ src/components/clientdash.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import "./clientdash.css"
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import './clientdash.css';
+import { useNavigate } from 'react-router-dom';
 
-export default function Clientdash(){
-    const navigate = useNavigate();
-    const submit=()=>{
-      navigate("/");
-    }
+export default function Clientdash() {
+  const navigate = useNavigate();
+
+  const submit = () => {
+    navigate('/');
+  };
+
   return (
-    <>
-    
-  <div className="bg-slide">
-  <button type="submit" onClick={submit} className="btn btn-primary button-3">
-    Log Out
-  </button>
-      <h1 className="h1-slide">Welcome Client</h1>
-     
-
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-12 bg-slide">
+          <button type="submit" onClick={submit} className="btn btn-primary button-3">
+            Log Out
+          </button>
+          <h1 className="h1-slide">Welcome Client</h1>
+        </div>
+      </div>
     </div>
-    </>
-    
-  )
+  );
 }
