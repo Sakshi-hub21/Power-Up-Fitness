53377f71df488df3c58002e5fb3193da82d941f7
Sakshi Shinde <82663545+Sakshi-hub21@users.noreply.github.com>
2023-12-10 12:46:57 +0530

    Update add.js

--- src/components/add.js
+++ src/components/add.js
@@ -49,6 +49,7 @@ export default function Add() {
 
   return (
     <>
+    <div className="container-fluid">
     <div className="body-f">
     <h1 className=" h1-add text-center mb-3">Add Client Data</h1>
     <div className="container-add d-flex align-items-center justify-content-center ">
@@ -124,6 +125,7 @@ export default function Add() {
     </form>
     </div>
     </div>
+            </div>
    
     
 
