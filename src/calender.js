e9399512da9cfb5868c3d55ee2ccd96b5052839b
Sakshi Shinde <82663545+Sakshi-hub21@users.noreply.github.com>
2023-12-10 15:36:42 +0530

    Update calender.js

--- src/calender.js
+++ src/calender.js
@@ -23,7 +23,7 @@ const CustomEvent = ({ event }) => {
     <div style={{ display: "flex", flexDirection: "column", height: eventHeight }}>
       <div ref={titleRef} style={{ display: "flex", flexDirection: "column" }}>
         <span>{event.clientName}</span>
-        <span style={{ fontSize: "1.2em", color: "white" }}>{event.time}</span>
+        <span style={{ fontSize: "1.0em", color: "white" }}>{event.time}</span>
       </div>
 
     </div>
